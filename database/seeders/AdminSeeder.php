41ccd2210fe6fc313291af4a8ca02890297940e7
Muhammad Rivaldi Fanani <muhammadrivaldifnni01@gmail.com>
2025-01-04 14:57:38 +0700

    Update seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'nama' => 'Admin',
+            'email' => 'admin@laporan-masyarakat.it',
+            'password' => bcrypt('password'),
+        ])->assignRole('admin');
+    }
+}
---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+
+    private $permissions = [
+        'dashboard' => [
+            'view'
+        ],
+        'user' => [
+            'view',
+            'create',
+            'update',
+            'delete',
+        ],
+        'laporan' => [
+            'view',
+            'create',
+            'update',
+            'delete',
+        ],
+        'alamat' => [
+            'view',
+            'create',
+            'update',
+            'delete',
+        ],
+        'role' => [
+            'view',
+            'create',
+            'update',
+            'delete',
+        ],
+        'kategori-laporan' => [
+            'view',
+            'create',
+            'update',
+            'delete',
+        ],
+    ];
+    public function run(): void
+    {
+        foreach ($this->permissions as $role => $value) {
+            foreach ($value as $permission) {
+                Permission::firstOrCreate([
+                    'name' => $role . '-' . $permission,
+                ]);
+            }
+        }
+
+        \Spatie\Permission\Models\Role::firstOrCreate([
+            'name' => 'admin',
+            'guard_name' => 'web'
+        ])->givePermissionTo(Permission::all());
+
+        \Spatie\Permission\Models\Role::firstOrCreate([
+            'name' => 'user',
+            'guard_name' => 'web'
+        ])->givePermissionTo([
+            'kategori-laporan-view',
+            'laporan-view',
+            'laporan-create',
+            'laporan-update',
+            'laporan-delete',
+        ]);
+    }
+}
